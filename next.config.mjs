c266c2f2ca4fe890d7bba7a7b08a0c1c56f2be89
Kevon Kibochi <devokrann@gmail.com>
2025-01-15 12:07:37 +0300

    todo: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -51,6 +51,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '**.pixabay.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 
